78f0cb55dace5e17f5654bae0b7ef8c840562c6c
anstill79 <61672433+anstill79@users.noreply.github.com>
2022-11-30 14:43:36 -0800

    commit

--- VEMPinterp/VEMPinterp.js
+++ VEMPinterp/VEMPinterp.js
@@ -1,1 +1,21 @@
 
+  const ctx = document.getElementById('VEMPinterp');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['No Suspicion', 'Low Suspicion', 'High Suspicion', 'Very High Suspicion'],
+      datasets: [{
+        label: '# of Votes',
+        data: [0, 1, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
