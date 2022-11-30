70e8a8506fa50936d1ac05da0f0a82a1507c4a30
anstill79 <61672433+anstill79@users.noreply.github.com>
2022-11-30 15:43:25 -0800

    commit

--- VEMPinterp/VEMPinterp.js
+++ VEMPinterp/VEMPinterp.js
@@ -1,41 +1,38 @@
 
-  const ctx_R = document.getElementById('VEMPchart_R');
-  const ctx_L = document.getElementById('VEMPchart_L');
+const ctx_R = document.getElementById('VEMPchart_R');
+const ctx_L = document.getElementById('VEMPchart_L');
 
-  new Chart(ctx_R, {
+const chartOptions = {
+    scales: {
+        y: {
+            beginAtZero: true
+        }
+    }
+}
+
+new Chart(ctx_R, {
     type: 'bar',
     data: {
-      labels: ['No Suspicion', 'Low Suspicion', 'High Suspicion', 'Very High Suspicion'],
-      datasets: [{
-        label: 'Right Score',
-        data: [0, 1, 2, 3],
-        borderWidth: 1
-      }]
+        labels: ['No Suspicion', 'Low Suspicion', 'High Suspicion', 'Very High Suspicion'],
+        datasets: [{
+            label: 'Right Score',
+            data: [0, 1, 2, 3],
+            borderWidth: 1
+        }]
     },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
-    }
-  });
+options: chartOptions
+});
 
-  new Chart(ctx_L, {
+
+new Chart(ctx_L, {
     type: 'bar',
     data: {
-      labels: ['No Suspicion', 'Low Suspicion', 'High Suspicion', 'Very High Suspicion'],
-      datasets: [{
-        label: 'Left Score',
-        data: [0, 1, 2, 3],
-        borderWidth: 1
-      }]
+        labels: ['No Suspicion', 'Low Suspicion', 'High Suspicion', 'Very High Suspicion'],
+        datasets: [{
+            label: 'Left Score',
+            data: [0, 1, 2, 3],
+            borderWidth: 1
+        }]
     },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
-    }
-  });
+    options: chartOptions
+});
