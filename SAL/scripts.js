dd3beb140ec2f6b6cf54f57c1bb3c24086428b94
anstill79 <61672433+anstill79@users.noreply.github.com>
2023-10-18 10:04:39 -0700

    Allow SAL when initial AC is mild, in case contra ear is too poor

--- SAL/scripts.js
+++ SAL/scripts.js
@@ -61,9 +61,9 @@ function doSAL() {
   const targetInfo = `${ear}_info_${freq}`;
   //why do SAL in this case?
   if (data[ear][freq][0] < 50 && data[ear][freq][0] !== null) {
-    SALresults[ear][freq][0] = null;
+    //SALresults[ear][freq][0] = null;
     SALresults[ear][freq][2] = '⚠️';
-    SALresults[ear][freq][1] = "Initial result is lower than 50dB. Traditional masking may be a better choice for this frequency.";
+    SALresults[ear][freq][1] = "Initial result is lower than 50dB. Traditional masking may be a better choice for this frequency. The SAL value is displayed in case traditional masking is not viable due to opposite ear severity.";
     setResultsToCell(ear, freq, targetResult, targetInfo);
     return;
   }
