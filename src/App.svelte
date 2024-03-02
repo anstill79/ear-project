88ab42789c93cc73de8a41b54aabd011f851c227
anstill79 <61672433+anstill79@users.noreply.github.com>
2024-03-01 18:33:20 -0800

    Create App.svelte

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Header from "./lib/Header.svelte";
+  import Section from "./lib/Section.svelte";
+  let title = "Ear Project";
+  let sectionName = "Sensorineural Acuity Level (SAL)";
+</script>
+
+<main>
+  <Header {title} />
+  <Section {sectionName} />
+</main>
