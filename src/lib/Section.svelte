51e521f809e401ac2b3718759035443741844f46
anstill79 <61672433+anstill79@users.noreply.github.com>
2024-03-01 18:30:56 -0800

    Create Section.svelte

---
+++ src/lib/Section.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let sectionName = "SAL";
+</script>
+
+<div id="sub_section">
+  <span>{sectionName}</span>
+</div>
+
+<style>
+  #sub_section {
+    display: grid;
+    place-content: center;
+    border: 1px solid black;
+    max-width: 900px;
+    margin: auto;
+    margin-top: 5px;
+  }
+</style>
